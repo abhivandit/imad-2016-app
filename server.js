5fbacc3df271439567c963f425c4cb2c1fd1cede
abhivandit <abhivandit@gmail.com>
2016-09-28 17:50:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -3,13 +3,78 @@ var morgan = require('morgan');
 var path = require('path');
 
 var app = express();
+var articles={
+    'article-one' :{
+        title : ' Abhis Article',
+        heading : 'my artcile',
+        first : 'jhbghs'  ,
+        content:`this is my file.
+        sahkjhashasbkjhaj
+        askjh `,
+    },
+    'artcle-two' : {
+        title :' abhis second article',
+        heading :'this is my world',
+        first :'ajhgvas',
+        content :`djknajkxdnakldjnklsajdxlkajsd
+        akljhxdlasjdkljasd
+        akjhdlaj`,
+    }
+};
+
+function createTemplate(data){
+    var title=data.title;
+    var heading=data.heading;
+    var first=data.first;
+    var content=data.content;
+    var htmlTemplate=`
+    <html>
+    <head>
+        <title>
+            ${title}
+        </title>
+           <link href="/ui/style.css" rel="stylesheet" />
+           <meta name="viewport" content="width=device-width, initial-scale=1"/>
+          
+    </head>
+    <body>
+        <div class="myfirsthtml">
+        <div>
+            <a href="/">Home</a>
+            
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${first}
+        </div>
+        <div>
+            ${content}
+        </div>
+        </div>
+        
+        
+    </body>
+</html>
+`;
+return htmlTemplate;
+    
+    
+}
+        
+        
+
+
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/article-one',function (req,res){
-   res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+app.get('/:articleName',function (req,res){
+    var articleName = req.params.articleName;
+   res.send(createTemplate(articles[articleName]));
 });
 app.get('/article-two',function (req,res){
       res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
