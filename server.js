617c2a5d6c28e4296be240aeeae1984c6e8e6841
abhivandit <abhivandit@gmail.com>
2016-10-01 19:37:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,15 +72,15 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/:articleName',function (req,res){
-    var articleName = req.params.articleName;
-   res.send(createTemplate(articles[articleName]));
-});
+//app.get('/:articleName',function (req,res){
+ //   var articleName = req.params.articleName;
+  /* res.send(createTemplate(articles[articleName]));
+});*/
 
 
-app.get('/article-three',function (req,res){
+/*app.get('/article-three',function (req,res){
      res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
+});*/
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
