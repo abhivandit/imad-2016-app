58a775732d1ae9a758f9745266bd828991fc0838
abhivandit <abhivandit@gmail.com>
2016-10-02 21:28:11 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,11 +3,11 @@ var element=document.getElementById('mytext');
 element.innerHTML = 'ajhdajkhd';
 var myimg=document.getElementById('madi');
 var marginLeft=0;
-function moveLeft(){
+function moveRight(){
     
-    myimg.style.marginLeft=(marginleft + 10)+'px';
+    myimg.style.marginLeft=(marginLeft + 10)+'px';
 }
 myimg.onclick = function(){
-var interval=setInterval(moveLeft,10);
+var interval = setInterval(moveRight, 10);
     
 };
