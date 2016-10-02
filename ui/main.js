75cce7cdb0beb0d52979cbc86b985c880c3aa2e3
abhivandit <abhivandit@gmail.com>
2016-10-02 19:28:16 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
 console.log('Loaded!');
+var element=document.getElementById('mytext');
+element.innerHTML = 'ajhdajkhd';
+var myimg=document.getElementById('madi');
+myimg.onclick = function(){
+    myimg.style.marginLeft= '100px';
+    
+};
