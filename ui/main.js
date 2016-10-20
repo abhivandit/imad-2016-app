72b8f930959154d3b37e066a69a95608b299c7a7
abhivandit <abhivandit@gmail.com>
2016-10-20 16:38:23 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,8 +16,19 @@ var interval = setInterval(moveRight, 50);
 var counter =0;
 var button=document.getElementById('counter');
 button.onclick = function(){
-    counter=counter+1;
-    var span=document.getElementById('count');
-    span.innerHTML=counter.toString();
+    var request=new XMLHttpRequest();
+    request.onReadyStateChange=function(){
+        if(request.readyState==XMLHttpRequest.DONE){
+            if(request.state==200){
+                var counter=request.responseText;
+                 var span=document.getElementById('count');
+                span.innerHTML=counter.toString();
+            }
+        }
+        
+        
+    };
+  //  counter=counter+1;
+   
     
 };
