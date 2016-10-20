f0979243301497d5f11dc2270cb5c816fe715c77
abhivandit <abhivandit@gmail.com>
2016-10-20 19:50:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -48,7 +48,7 @@ submit.onclick=function(){
            
            names=JSON.parse(names);//vvi
            var list='';
-           for(var i=0;i<names.length;i++){
+           for(var i=0;i < names.length ;i++){
                list+='<li>'+names[i]+'</li>';
                
            }
